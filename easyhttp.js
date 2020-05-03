5fb2ca8e7950d9be92ec0c96967ec5741512cacf
wanlab-io <hello.wanlab@gmail.com>
2020-05-03 13:10:29 +0800

    get request method

---
+++ easyhttp.js
@@ -0,0 +1,74 @@
+/**
+ * EasyHTTP Library
+ * Library for making HTTP requests
+ *
+ * @version 3.0.0
+ * @author  Wan @ Malaynium Lab
+ * @license MIT
+ *
+ **/
+
+class EasyHTTP {
+  
+  // HTTP Get Request
+ async get(url) {
+   const response = await fetch(url);
+   const resData = await response.json();
+   return resData;
+  }
+
+  // // HTTP Post Request
+  // post( url, data ){
+  //   return new Promise((resolve, reject) => {
+  //     fetch(url, {
+  //       method: 'POST',
+  //       headers: {
+  //         'Content-type': 'application/json'
+  //       },
+
+  //       body: JSON.stringify(data)
+  //     })
+
+  //     .then(res => res.json())
+  //     .then(data => resolve(data))
+  //     .catch(err => reject(err));
+  //   });
+  // }
+
+  // HTTP Put Request
+  // put( url, data ){
+  //   return new Promise((resolve, reject) => {
+  //     fetch(url, {
+  //       method: 'PUT',
+  //       headers: {
+  //         'Content-type': 'application/json'
+  //       },
+
+  //       body: JSON.stringify(data)
+  //     })
+
+  //     .then(res => res.json())
+  //     .then(data => resolve(data))
+  //     .catch(err => reject(err));
+  //   });
+  // }
+
+ // HTTP DELEE Request
+//  delete(url) {
+//   return new Promise((resolve, reject) => {
+//     fetch(url, {
+//       method: 'DELETE',
+//         headers: {
+//           'Content-type': 'application/json'
+//         },
+//     })
+
+//     .then(res => res.json())
+//     .then(() => resolve('Resource Deleted...'))
+//     .catch(err => reject(err)); 
+//   });
+// }
+
+} // end of class
+
+
